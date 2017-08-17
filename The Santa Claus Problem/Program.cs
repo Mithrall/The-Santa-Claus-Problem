0b3b6f465feb1ae286bb453bfd013babff30c5e6
Mithrall <kenneth-dorn@hotmail.com>
2017-08-17 16:51:50 +0200

    so many bugs still :/

--- The Santa Claus Problem/Program.cs
+++ The Santa Claus Problem/Program.cs
@@ -2,11 +2,131 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace The_Santa_Claus_Problem {
     class Program {
+        static Thread[] threads = new Thread[51];
+
+        static int elfCount = 0;
+        static int reindeerCount = 0;
+
+        static Semaphore santaSem = new Semaphore(0,1);             //Where Santa waits
+        static Semaphore reindeerSem = new Semaphore(0,9);          //Where reindeer wait
+        static Semaphore elfSem = new Semaphore(0,10);       //Where elves wait
+
+        static Mutex m = new Mutex();           //Protects the counters
+        static Mutex elfMutex = new Mutex();    //blocks other elves while three
+                                                //elves are being helped
+
         static void Main(string[] args) {
+            Random rnd = new Random();
+            string type;
+            threads[0] = new Thread(santaLoop);
+            threads[0].Start();
+
+            for (int i = 1; i < threads.Length-1; i++) {
+
+                threads[i] = new Thread(ThreadProcess);
+                type = rnd.Next(2).ToString();
+                switch (type) {
+                    case "0":
+                        threads[i].Name = "reindeer";
+                        threads[i].Start();
+                        break;
+                    case "1":
+                        threads[i].Name = "elf";
+                        threads[i].Start();
+                        break;
+                    default:
+                        break;
+                }
+            }
+            Console.Read();
+        }
+
+        private static void santaLoop() {
+            while (true) {
+                Console.WriteLine("Santa is sleeping");
+                santaSem.WaitOne();
+                Thread.Sleep(1000);
+                Console.WriteLine("Santa has been woken up");
+                m.WaitOne();
+                Thread.Sleep(1000);
+                if (reindeerCount == 9) {
+                    prepSleigh();
+                    reindeerSem.Release(9);
+                    reindeerCount = 0;
+                } else {
+                    elfSem.Release(3);
+                    helpElves();
+                }
+                Thread.Sleep(1000);
+                m.ReleaseMutex();
+            }
+        }
+
+        private static void helpElves() {
+        }
+
+        private static void prepSleigh() {
+            Console.WriteLine("Santa prepped the sleigh");
+        }
+
+        static void ThreadProcess() {
+            //if elf
+            if (Thread.CurrentThread.Name == "elf") {
+                Console.WriteLine("An elf is waiting at the door");
+                elfMutex.WaitOne();
+                Thread.Sleep(1000);
+                Console.WriteLine("An elf is waiting in line, closing door");
+                m.WaitOne();
+                Thread.Sleep(1000);
+                elfCount++;
+                if (elfCount == 3) {
+                    Console.WriteLine(elfCount + " elfs are waiting for help, signaling Santa");
+                    santaSem.Release();
+                } else {
+                    Console.WriteLine(elfCount + " elfs are waiting for help, opening door");
+                    elfMutex.ReleaseMutex();
+                }
+                Thread.Sleep(1000);
+                m.ReleaseMutex();
+                elfSem.WaitOne();
+                getHelp();
+                m.WaitOne();
+                elfCount--;
+                if (elfCount == 0) {
+                    Console.WriteLine(elfCount + "Are still waiting for help");
+                    elfMutex.ReleaseMutex();
+                }
+                Thread.Sleep(1000);
+                m.ReleaseMutex();
+
+            //if reindeer
+            } else {
+                Console.WriteLine("A reindeer is waiting in line");
+                m.WaitOne();
+                Thread.Sleep(1000);
+                reindeerCount++;
+                if (reindeerCount == 9) {
+                    Console.WriteLine(reindeerCount + " (all) reindeers are ready, signaling Santa");
+                    santaSem.Release();
+                } else Console.WriteLine(reindeerCount + " Reindeers are ready");
+                m.ReleaseMutex();
+                Thread.Sleep(1000);                
+                reindeerSem.WaitOne();
+                getHitched();
+            }
+        }
+
+        private static void getHelp() {
+            Console.WriteLine("an elf are getting help");
+        }
+
+        private static void getHitched() {
+            Console.WriteLine("a reindeer are hitched");
         }
     }
 }
